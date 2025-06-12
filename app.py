f93fb35c63a40186d0932ecb98dabe13cf7d3e4c
GoldAndriuks <eh2302is387@ueh.edu.mx>
2025-06-12 08:57:28 -0600

    opcion select

--- app.py
+++ app.py
@@ -8,27 +8,27 @@ app = Flask(__name__)
 def formulario():
     if request.method == 'POST':
         nombre = request.form['nombre']
-        apellido =request.form['apellido']
+        apellido = request.form['apellido']
+        correo = request.form['correo']
         contraseña = request.form['contraseña']
         telefono = request.form['telefono']
         ciudad = request.form['ciudad']
+        CP = request.form['CP']
+        estado = request.form['estado']
 
-        
         es_nuevo = not os.path.exists('resultados.csv') or os.stat('resultados.csv').st_size == 0
 
-        
         with open('resultados.csv', mode='a', newline='', encoding='utf-8') as archivo:
             escritor = csv.writer(archivo)
             if es_nuevo:
-                escritor.writerow(['Nombre', 'Contraseña', 'Teléfono', 'Escuela'])
-            escritor.writerow([nombre, contraseña, telefono, ciudad , apellido])
+                escritor.writerow(['Nombre', 'Apellido', 'Correo', 'Contraseña', 'Teléfono', 'Ciudad', 'CP', 'Estado'])
+            escritor.writerow([nombre, apellido, correo, contraseña, telefono, ciudad, CP, estado])
 
         return f"""
         <h2>¡Registro exitoso!</h2>
-        
         <a href="/">Volver</a>
         """
-    
+
     return render_template('index.html')
 
 if __name__ == '__main__':
